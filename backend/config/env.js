7ac56432790544451a33081732216b7ffb2beea5
devanshgargofficial <136746503+devanshgargofficial@users.noreply.github.com>
2025-01-27 01:19:59 +0530

    Delete backend/config/env.js

--- backend/config/env.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const PORT = process.env.PORT || 5000;
-export const DB_URI = process.env.DB_URI || "mongodb+srv://gargdevansh999:Devansh%401@referralwebsite.cawtx.mongodb.net/?retryWrites=true&w=majority&appName=ReferralWebsite";
